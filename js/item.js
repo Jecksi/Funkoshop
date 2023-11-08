e85154be08b7b0542709ff1ba7a5a482e002b0d7
ProyectosMarcos <m-fernandez988@hotmail.com>
2023-11-07 22:44:18 -0300

    actualizacion

--- js/item.js
+++ js/item.js
@@ -4,3 +4,4 @@ const quantity = document.querySelector('#quantity');
 
 add.addEventListener('click',() => quantity.value = Number(quantity.value) + 1);
 subtract.addEventListener('click',() => quantity.value = Number(quantity.value) - 1);
+
