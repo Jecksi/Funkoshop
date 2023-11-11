c1df71d0c8a77d9857131c0a37360e898cba26be
ProyectosMarcos <m-fernandez988@hotmail.com>
2023-11-10 23:05:01 -0300

    actualizacion

--- js/item.js
+++ js/item.js
@@ -1,12 +1,33 @@
+// eslint-disable-next-line no-undef
 const add = document.querySelector('#add');
+// eslint-disable-next-line no-undef
 const subtract = document.querySelector('#subtract');
+// eslint-disable-next-line no-undef
 const quantity = document.querySelector('#quantity');
+// eslint-disable-next-line camelcase, no-unused-vars
+
+const add_2 = document.querySelector('#add_2');
+subtract_2 = document.querySelector('#subtract_2');
+const quantity_2 = document.querySelector('#quantity_2');
 
 add.addEventListener(
   'click',
-  () => (quantity.value = Number(quantity.value) + 1)
+  // eslint-disable-next-line no-return-assign
+  () => (quantity.value = Number(quantity.value) + 1),
 );
 subtract.addEventListener(
   'click',
-  () => (quantity.value = Number(quantity.value) - 1)
+  // eslint-disable-next-line no-return-assign
+  () => (quantity.value = Number(quantity.value) - 1),
+);
+
+add_2.addEventListener(
+  'click',
+  // eslint-disable-next-line no-return-assign
+  () => (quantity_2.value = Number(quantity_2.value) + 1),
+);
+subtract_2.addEventListener(
+  'click',
+  // eslint-disable-next-line no-return-assign
+  () => (quantity_2.value = Number(quantity_2.value) - 1),
 );
